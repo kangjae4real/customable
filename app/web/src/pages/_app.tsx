b166638fb7f58039a195fe912cef9997202e799a
Kangjae Choi <awpe89@gmail.com>
2023-12-03 17:10:33 +0900

    chore(web): add packages

--- app/web/src/pages/_app.tsx
+++ app/web/src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@emotion/react";
 import { THEME } from "@/styles/theme";
+import GlobalStyle from "@/styles/GlobalStyle";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={THEME}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={THEME}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
